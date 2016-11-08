52bc87aa71825f0f3571d0e5bb7a7af65fc7147a
Jan Schiefer <jschiefer@users.noreply.github.com>
2016-11-08 01:29:54 -0800

    Try out some p/invoke

--- Program.fs
+++ Program.fs
@@ -1,8 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Runtime.InteropServices
+
+[<DllImport("librtlsdr")>]
+RTLSDR_API uint32_t rtlsdr_get_device_count()
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Hello World!"
+    let count = rtlsdr_get_device_count()
+    printfn "We have %d devices" count
     0 // return an integer exit code
